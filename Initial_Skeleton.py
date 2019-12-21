d151399b8e9a678111f0cba825157323662ea289
Gawasinspire <gavaskar2018@gmail.com>
2019-12-21 12:41:00 +0100

    Implement Skeleton with updated Functions

--- Initial_Skeleton.py
+++ Initial_Skeleton.py
@@ -1,7 +1,12 @@
 #headers
 
+
 #Functions
 
+    #Generate cars in random 1 to 5 min 
+    #timer of dt 5 seconds interval
+    #function to change light
+    #Detector to check comming car
 
 #main Drivers
 
